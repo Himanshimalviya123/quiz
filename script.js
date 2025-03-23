00b7234a61ff77876515dc2d6f0e75a6b396e5bb
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-03-24 00:24:05 +0530

    update

--- script.js
+++ script.js
@@ -7,46 +7,57 @@ const quizSection = document.querySelector('.quiz-section');
 const quizBox = document.querySelector('.quiz-box');
 
  
-// startBtn.onclick = ()=>{
-//     popupInfo.classList.add('active');
-//     main.classList.add('active');
-// }
-let start=()=>{
+startBtn.onclick = ()=>{
     popupInfo.classList.add('active');
-     main.classList.add('active');
+    main.classList.add('active');
 }
-let cont= ()=>{
-    quizSection.classList.add('active');
-    popupInfo.classList.remove('active');
-    main.classList.remove('active');
-    quizBtn.classList.add('active');
-}
-let exit = ()=>{
-    popupInfo.classList.remove('active');
-    main.classList.remove('active');
-}
-// exitBtn.onclick = ()=>{
-//     popupInfo.classList.remove('active');
-//     main.classList.remove('active');
+
+//  const start=()=>{
+//     popupInfo.classList.add('active');
+//      main.classList.add('active');
 // }
-// continueBtn.onclick = ()=>{
+// const cont= ()=>{
 //     quizSection.classList.add('active');
 //     popupInfo.classList.remove('active');
 //     main.classList.remove('active');
 //     quizBtn.classList.add('active');
-//     showQuestion(0);
 // }
+// const  exit = ()=>{
+//     popupInfo.classList.remove('active');
+//     main.classList.remove('active');
+// }
+exitBtn.onclick = ()=>{
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
+}
+continueBtn.onclick = ()=>{
+    quizSection.classList.add('active');
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
+    quizBtn.classList.add('active');
+    showQuestion(0);
+    questioncounter(1);
+}
 // ==========================================================================
 let questioncount=0;
+let questionnumb=1;
 const nextBtn=document.querySelector('.next-btn');
 nextBtn.onclick =()=>{
-    questioncount++;
-    showQuestion(questioncount);
-
+    if(questioncount< questions.length-1){
+        questioncount++;
+        showQuestion(questioncount);
+        questionnumb++;
+        questioncounter(questionnumb);
+    
+    }
+    else{
+        console.log('question completed');
+        
+    }
 }
-const optionList=document.querySelector('.option-list';
+const optionList=document.querySelector('.option-list');
 
-)
+
 function showQuestion(index){
     const questionText=document.querySelector('.question-text');
     questionText.textContent=`${questions[index].numb}.${questions[index].question}`
@@ -55,9 +66,31 @@ function showQuestion(index){
     <div class="option"><span>${questions[index].options[2]}</span></div>
     <div class="option"><span>${questions[index].options[3]}</span></div>`;
     optionList.innerHTML=optionTag;
+    const option=document.querySelector('.option');
+    for(Let i=0; i <option.length; i++)
+    {
+        option[i].setAttribute('onclick','optionselected(this)');
+    }
 }
+function optionselected(answer)
+{
+    let userAnswer=answer.textContent;
+    let correctAnswer=questions[questioncount].answer;
+    if(userAnswer==correctAnswer)
+    {
+        console.log('answer is correct');
+        answer.classList.add('correct');
 
-
+        
+    }
+    else{
+        console.log('answer is wrong');
+        
+    }
+}
+function questioncounter(index){
+    const questionTotal=document.querySelector('.question-tatal')
+}
 
 
 
