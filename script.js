ab2af8a9fd1cff3a5f8f1f0bdd12537ff34d8f2d
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-03-23 23:45:36 +0530

    modify

--- script.js
+++ script.js
@@ -7,18 +7,78 @@ const quizSection = document.querySelector('.quiz-section');
 const quizBox = document.querySelector('.quiz-box');
 
  
-startBtn.onclick = ()=>{
+// startBtn.onclick = ()=>{
+//     popupInfo.classList.add('active');
+//     main.classList.add('active');
+// }
+let start=()=>{
     popupInfo.classList.add('active');
-    main.classList.add('active');
+     main.classList.add('active');
 }
-
-exitBtn.onclick = ()=>{
-    popupInfo.classList.remove('active');
-    main.classList.remove('active');
-}
-continueBtn.onclick = ()=>{
+let cont= ()=>{
     quizSection.classList.add('active');
     popupInfo.classList.remove('active');
     main.classList.remove('active');
-    quizBox.classList.add('active');
+    quizBtn.classList.add('active');
 }
+let exit = ()=>{
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
+}
+// exitBtn.onclick = ()=>{
+//     popupInfo.classList.remove('active');
+//     main.classList.remove('active');
+// }
+// continueBtn.onclick = ()=>{
+//     quizSection.classList.add('active');
+//     popupInfo.classList.remove('active');
+//     main.classList.remove('active');
+//     quizBtn.classList.add('active');
+//     showQuestion(0);
+// }
+// ==========================================================================
+let questioncount=0;
+const nextBtn=document.querySelector('.next-btn');
+nextBtn.onclick =()=>{
+    questioncount++;
+    showQuestion(questioncount);
+
+}
+const optionList=document.querySelector('.option-list';
+
+)
+function showQuestion(index){
+    const questionText=document.querySelector('.question-text');
+    questionText.textContent=`${questions[index].numb}.${questions[index].question}`
+    let optionTag=` <div class="option"><span>${questions[index].options[0]}</span></div>
+    <div class="option"><span>${questions[index].options[1]}</span></div>
+    <div class="option"><span>${questions[index].options[2]}</span></div>
+    <div class="option"><span>${questions[index].options[3]}</span></div>`;
+    optionList.innerHTML=optionTag;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
