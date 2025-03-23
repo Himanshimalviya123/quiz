2db27d0aba0655755475c271190019fa80d500d4
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-03-23 22:17:11 +0530

    update

---
+++ question.js
@@ -0,0 +1,63 @@
+let question=[
+    {
+    numb:1,
+    question:"what does html for?",
+    answer:"C. hyper taxt markup language",
+    options:[
+        "A. hyper type multi language",
+        "B. hyper text multiple language",
+        "C. hyper text markup language",
+        "D. home text markup language",
+
+    ]
+    },
+    {
+        numb:2,
+        question:"what does css stand for?",
+        answer:"A. cascading style sheet",
+        options:[
+            "A.  cascading style sheet",
+            "B. cute style sheet",
+            "C. computer style sheet",
+            "D. codehal style sheet",
+    
+        ]
+        },
+        {
+            numb:3,
+            question:"what does php stand for?",
+            answer:"B.hypertext preprocessor",
+            options:[
+                "A. hypertext preprogramming",
+                "B. hypertext preprocessor",
+                "C. programing hypertext preprocessor",
+                "D. hypertext programming",
+        
+            ]
+            },
+            {
+                numb:4,
+                question:"what does sql stand for?",
+                answer:"D. structured query language",
+                options:[
+                    "A. strength query language",
+                    "B. strength query language",
+                    "C. science question  language",
+                    "D. structured query language",
+            
+                ]
+                },
+                {
+                    numb:5,
+                    question:"what does xml for?",
+                    answer:"A. extensible markup language",
+                    options:[
+                        "A.  extensible markup language",
+                        "B. explore multiple language ",
+                        "C. extra markup language",
+                        "D. excellent multiple language",
+                
+                    ]
+                    },
+                
+]
